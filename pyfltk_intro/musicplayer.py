8c332dbbc8212178c563be63b499c11543abe8be
nick-hu <nicholas.squishy@gmail.com>
2013-09-30 13:47:37 -0700

    Update 30/09/2013 WIP

--- pyfltk_intro/musicplayer.py
+++ pyfltk_intro/musicplayer.py
@@ -1,19 +1,47 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-import os
+import subprocess as subp
 
 from fltk import *
 
-def get_fname(wid):
-    music = wid.value()
 
-mpath = "/home/inter/music/"
+def playmusic(wid):
+    mpath = "/home/inter/music/"
+    fname = fl_file_chooser("Pick a music file", "*.ogg", mpath)
+    ps.append(subp.Popen(["ogg123", fname]))
+    wid.deactivate()
+    pausebut.activate()
+    stopbut.activate()
 
-fname = Fl_File_Chooser(mpath, "*.ogg", FL_SINGLE, "Pick a music file")
-fname.callback(get_fname)
 
-fname.show()
+def pausemusic(wid):
+    pass
 
+
+def stopmusic(wid):
+    ps.pop().kill()
+    playbut.activate()
+    pausebut.deactivate()
+    stopbut.deactivate()
+
+ps = []
+
+win = Fl_Window(200, 200, 160, 60)
+
+win.begin()
+
+playbut = Fl_Button(10, 10, 40, 40, "@+4>")
+playbut.callback(playmusic)
+pausebut = Fl_Button(60, 10, 40, 40, "@circle")
+pausebut.callback(pausemusic)
+pausebut.deactivate()
+stopbut = Fl_Button(110, 10, 40, 40, "@square")
+stopbut.callback(stopmusic)
+stopbut.deactivate()
+
+win.end()
+
+win.show()
+Fl.scheme("gtk+")
 Fl.run()
-
-os.system("ogg123 " + music)
