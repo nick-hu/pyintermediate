d218a5318e7c05dddf40a2a86baeb34b5fe472ad
nick-hu <nicholas.squishy@gmail.com>
2013-10-10 13:48:30 -0700

    phonebook: fancy features

--- pyfltk_intro/phonebook.py
+++ pyfltk_intro/phonebook.py
@@ -8,46 +8,44 @@ from fltk import *
 
 def but_cb(wid, fname):
     bname = wid.label()
+    brow.clear()
+    numval.value("")
 
     if bname == "&Load":
         fname[0] = fl_file_chooser("Pick a database file", "*.txt", PATH)
         if fname[0]:
+            tbook.clear()
             try:
                 with open(fname[0]) as data:
                     tbook.update(cPickle.load(data))
             except:
-                tbook.clear()
+                brow.add("ERROR: Invalid database file")
+                fname[0] = ""
 
-    elif tbook:
-        if bname == "&Save as":
-            sname = fl_input("Save to file:", fname[0])
-            if sname:
-                with open(sname, "w") as data:
-                    cPickle.dump(tbook, data)
-        elif bname == "&Add/Edit":
-            name = fl_input("Enter name to add/edit:", brow.text(brow.value()))
-            if not name:
-                return
+    elif bname == "&Add/Edit" and fname[0]:
+        name = fl_input("Enter name to add/edit:", brow.text(brow.value()))
+        if name:
             num = fl_input("Enter number:")
-            if name and num:
+            if num:
                 tbook[name] = num
-        elif bname == "&Remove":
-            name = fl_input("Enter name to remove:", brow.text(brow.value()))
-            if name:
-                del tbook[name]
+    elif bname == "&Save/New" and fname[0]:
+        sname = fl_input("Save to file / Create new file:", fname[0])
+        if sname:
+            with open(sname, "w") as data:
+                cPickle.dump(tbook, data)
+    elif bname == "&Remove" and brow.value():
+        del tbook[brow.text(brow.value())]
 
-    brow.clear()
-    numval.value("")
-    for name in tbook.keys():
+    for name in sorted(tbook.iterkeys()):
         brow.add(name)
 
 
 def get_num(wid):
-    if wid.value():
+    if wid.value() and fname[0]:
         numval.value(tbook[brow.text(brow.value())])
 
 tbook = {}
-PATH, fname = path.dirname(path.abspath(__file__)), [""]
+PATH, fname = path.dirname(path.abspath(__file__)), [" "]
 col = fl_rgb_color
 
 win = Fl_Window(200, 200, 500, 500, "Phone Book")
@@ -55,12 +53,14 @@ win.color(FL_WHITE)
 
 win.begin()
 
-but_labels = ["&Load", "&Save as", "&Add/Edit", "&Remove"]
+but_labels = ["&Load", "&Save/New", "&Add/Edit", "&Remove"]
 but_colors = [col(80, 190, 50), col(240, 185, 0), col(50, 160, 255),
               col(215, 55, 10)]
+but_colors.extend(but_colors[::-1])
+
 for x in xrange(4):
     but = Fl_Button(10 + 121 * x, 10, 115, 30, but_labels.pop(0))
-    but.color(but_colors.pop(0))
+    but.color(but_colors.pop(0), but_colors.pop(-1))
     but.callback(but_cb, fname)
 
 brow = Fl_Hold_Browser(10, 50, 480, 400)
