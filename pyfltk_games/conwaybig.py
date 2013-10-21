1142ac41c7233fcb31459d3dbff2a4bff655469b
nick-hu <nicholas.squishy@gmail.com>
2013-10-21 13:46:27 -0700

    conway: 200x200 grid :D

--- pyfltk_games/conwaybig.py
+++ pyfltk_games/conwaybig.py
@@ -23,30 +23,24 @@ def but_cb(wid):
         update_grid(name)
     elif name == "Sto&p":
         Fl.remove_timeout(update_grid)
-    elif name == "&Clear":
+    else:
         for cell in cells:
             if cell.color() != FL_BLACK:
                 cell.color(FL_WHITE)
                 cell.state = False
                 cell.redraw()
         genbox.value(0)
-    else:
-        boxtype = FL_THIN_DOWN_BOX if wid.value() else FL_FLAT_BOX
-        for cell in cells:
-            if cell.color() != FL_BLACK:
-                cell.box(boxtype)
-                cell.redraw()
 
 
 def update_grid(name="&Start"):
-    nalive, neighbours = [0] * 83, (-83, -82, -81, -1, 1, 81, 82, 83)
+    nalive, neighbours = [0] * 203, (-203, -202, -201, -1, 1, 201, 202, 203)
     check = set([])
-    for cell in xrange(83, 6641):
+    for cell in xrange(203, 40601):
         n = [cells[cell + addr].state for addr in neighbours].count(True)
         nalive.append(n)
         if n > 0:
             check = check.union(set([cell + addr for addr in neighbours]))
-    nalive = nalive + [0] * 83
+    nalive = nalive + [0] * 203
 
     for cell in check:
         if cells[cell].color() != FL_BLACK:
@@ -65,38 +59,36 @@ def update_grid(name="&Start"):
 
 cells = []
 
-win = Fl_Window(200, 200, 940, 820, "Conway's Game of Life")
+win = Fl_Window(200, 200, 940, 808, "Conway's Game of Life")
 win.color(FL_WHITE)
 
 win.begin()
 
-for y in xrange(82):
-    for x in xrange(82):
-        but = Cell(x * 10, y * 10, 10, 10)
+for y in xrange(202):
+    for x in xrange(202):
+        but = Cell(x * 4, y * 4, 4, 4)
         but.box(FL_FLAT_BOX)
         but.color(FL_WHITE)
-        if y == 0 or y == 81 or x == 0 or x == 81:
+        if y == 0 or y == 201 or x == 0 or x == 201:
             but.color(FL_BLACK)
         else:
             but.callback(cell_cb)
         cells.append(but)
 
-but_labels = ["&Start", "Sto&p", "S&tep", "&Clear", "Show &grid"]
-for y in xrange(5):
+but_labels = ["&Start", "Sto&p", "S&tep", "&Clear"]
+for y in xrange(4):
     but = Fl_Button(830, 10 + y * 40, 100, 30)
     but.label(but_labels.pop(0))
-    if but.label() == "Show &grid":
-        but.type(FL_TOGGLE_BUTTON)
     but.callback(but_cb)
 
-speed = Fl_Value_Slider(830, 225, 100, 20, "Speed")
+speed = Fl_Value_Slider(830, 195, 100, 20, "Speed")
 speed.type(FL_HOR_SLIDER)
 speed.align(FL_ALIGN_TOP)
-speed.value(0.05)
-speed.minimum(0.05)
+speed.value(0.10)
+speed.minimum(0.10)
 speed.step(0.05)
 
-genbox = Fl_Value_Output(830, 300, 100, 30, "Generation")
+genbox = Fl_Value_Output(830, 270, 100, 30, "Generation")
 genbox.color(FL_WHITE)
 genbox.align(FL_ALIGN_TOP)
 
